539c80e9b835d833d6eb86b88e764d783cfe3b98
janickkiy <janickiy@mail.ru>
2026-06-18 21:32:02 +0300

    рефактринг кода

--- resources/views/frontend/info.blade.php
+++ resources/views/frontend/info.blade.php
@@ -7,19 +7,6 @@
 @section('keywords', $link->keywords)
 
 @section('content')
-    <div class="ad-card">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-2243538192217050"
-             data-ad-slot="8369734756"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
-
     <div class="content-layout">
         <div class="content-main">
             <article class="content-card">
